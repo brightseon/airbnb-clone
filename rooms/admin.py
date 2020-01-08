c0709f0e0cabfee8257a1b5297da4c7a4b0b9944
seon <seonu0823@gmail.com>
2020-01-08 19:45:19 +0900

    #4-3 ManyToMany like a Boss

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
