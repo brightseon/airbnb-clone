d8f5c65d9c005441312a22ba41aa8d3af4e03580
seon <seonu0823@gmail.com>
2020-01-11 11:22:42 +0900

    #5-0 Review Model

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,8 @@ THIRD_PARTY_APPS = [
 PROJECT_APPS = [
     'core.apps.CoreConfig',
     'users.apps.UsersConfig',
-    'rooms.apps.RoomsConfig'
+    'rooms.apps.RoomsConfig',
+    'reviews.apps.ReviewsConfig'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
