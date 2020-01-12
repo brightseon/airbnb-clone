04d0f19f193850424017a63db97c3d189662a612
seon <seonu0823@gmail.com>
2020-01-12 20:18:25 +0900

    #5-1 Reservations Model

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,8 @@ PROJECT_APPS = [
     'core.apps.CoreConfig',
     'users.apps.UsersConfig',
     'rooms.apps.RoomsConfig',
-    'reviews.apps.ReviewsConfig'
+    'reviews.apps.ReviewsConfig',
+    'reservations.apps.ReservationsConfig'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
