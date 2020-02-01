f8a9abdb00ecf35f9dca6344ebafe1c9092e0264
seon <seonu0823@gmail.com>
2020-02-01 22:14:28 +0900

    #7-2 Many to Many _set

--- rooms/models.py
+++ rooms/models.py
@@ -72,10 +72,10 @@ class Room(core_models.TimeStampedModel):
     # 바로예약
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey('users.User', related_name='rooms', on_delete=models.CASCADE)
-    room_type = models.ForeignKey('RoomType', on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField('Amenity', blank=True)
-    facilities = models.ManyToManyField('Facility', blank=True)
-    house_rules = models.ManyToManyField('HouseRule', blank=True)
+    room_type = models.ForeignKey('RoomType', related_name='rooms', on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField('Amenity', related_name='rooms' blank=True)
+    facilities = models.ManyToManyField('Facility', related_name='rooms' blank=True)
+    house_rules = models.ManyToManyField('HouseRule', related_name='rooms' blank=True)
 
     def __str__(self):
         return self.name
