8a2ef9dde52aa6f6f5e0a31064c21dd7720f1b00
seon <seonu0823@gmail.com>
2020-02-09 12:04:06 +0900

    #8-0 Review Admin and Room Average

--- rooms/models.py
+++ rooms/models.py
@@ -73,9 +73,17 @@ class Room(core_models.TimeStampedModel):
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey('users.User', related_name='rooms', on_delete=models.CASCADE)
     room_type = models.ForeignKey('RoomType', related_name='rooms', on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField('Amenity', related_name='rooms' blank=True)
-    facilities = models.ManyToManyField('Facility', related_name='rooms' blank=True)
-    house_rules = models.ManyToManyField('HouseRule', related_name='rooms' blank=True)
+    amenities = models.ManyToManyField('Amenity', related_name='rooms', blank=True)
+    facilities = models.ManyToManyField('Facility', related_name='rooms', blank=True)
+    house_rules = models.ManyToManyField('HouseRule', related_name='rooms', blank=True)
 
     def __str__(self):
         return self.name
+
+    def total_rating(self) :
+        all_reviews = self.reviews.all()
+        all_ratings = []
+
+        for review in all_reviews :
+            all_ratings.append(review.rating_average())
+        
