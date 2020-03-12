2b4f71258aa16c28e428653c5e8774533cc1be6e
seon <seonu0823@gmail.com>
2020-03-12 23:56:38 +0900

    #9-3 seed_rooms part One

--- rooms/models.py
+++ rooms/models.py
@@ -88,8 +88,9 @@ class Room(core_models.TimeStampedModel):
         all_reviews = self.reviews.all()
         all_ratings = 0
 
-        for review in all_reviews:
-            all_ratings += review.rating_average()
+        if len(all_reviews) > 0 :
+            for review in all_reviews:
+                all_ratings += review.rating_average()
 
-        return all_ratings / len(all_reviews)
-        
+            return all_ratings / len(all_reviews)
+        return 0
