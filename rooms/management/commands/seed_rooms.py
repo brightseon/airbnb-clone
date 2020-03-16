09e1b7ce68c3b60ac102605d2f121722ed45891d
seon <seonu0823@gmail.com>
2020-03-16 22:10:59 +0900

    #9-4 seed_rooms part Two

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,5 +1,6 @@
 import random
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
 from django_seed import Seed
 from rooms import models as room_models
 from users import models as user_models
@@ -7,15 +8,15 @@ from users import models as user_models
 
 class Command(BaseCommand):
 
-    help = "This command creates amenities"
+    help = 'This command creates amenities'
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--number", default = 2, type = int, help="How many rooms you want to create"
+            '--number', default = 2, type = int, help='How many rooms you want to create'
         )
 
     def handle(self, *args, **options):
-        number = options.get("number")
+        number = options.get('number')
         seeder = Seed.seeder()
         all_users = user_models.User.objects.all()
         room_types = room_models.RoomType.objects.all()
@@ -33,5 +34,17 @@ class Command(BaseCommand):
                 'baths': lambda x: random.randint(1, 5),
             },
         )
-        seeder.execute()
-        self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+
+        for pk in created_clean:
+            photo = room_models.Room.objects.get(pk=pk)
+            
+            for i in range(3, random.randint(10, 17)):
+                room_models.Photo.objects.create(
+                    caption = seeder.faker.sentence(),
+                    room = rooom,
+                    file = f'/media/{ random.randint(1, 31) }.webp'
+                )
+
+        self.stdout.write(self.style.SUCCESS(f'{number} rooms created!'))
