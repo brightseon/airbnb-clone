6d995283fe01203f200434841bf2972bd574f460
seon <seonu0823@gmail.com>
2020-04-03 22:00:23 +0900

    #10-0 Introduction to Urls and Views

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path('admin/', admin.site.urls)]
+urlpatterns = [path('', include('core.urls', namespace="core")), path('admin/', admin.site.urls)]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
