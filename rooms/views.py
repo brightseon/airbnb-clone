57e2aa5734946885c4bcbde293f34fc5ce750488
seon <seonu0823@gmail.com>
2020-04-15 16:48:34 +0900

    #11-3 Next Previous Page Navigation

--- rooms/views.py
+++ rooms/views.py
@@ -6,16 +6,10 @@ def all_rooms(request):
     page = request.GET.get('page', 1)
     page = int(page or 1)
     page_size = 10
-    print('page_size : ', page_size)
     limit = page_size * page
-    print('limit : ', limit)
     offset = limit - page_size
-    print('offset : ', offset)
     all_rooms = models.Room.objects.all()[offset:limit]
-    print('all_rooms : ', all_rooms)
     page_count = ceil(models.Room.objects.count() / page_size)
-    print('page_count : ', page_count)
-    print('page_range : ', range(1, page_count))
 
     return render(request, 'rooms/home.html', {
         'rooms' : all_rooms,
