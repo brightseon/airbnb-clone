8a68b96f653ff63694c0d8f4a37eee6710bbc73f
seon <seonu0823@gmail.com>
2020-06-01 22:21:14 +0900

    #13-6 Filtering Like a Boss part Tow

--- rooms/views.py
+++ rooms/views.py
@@ -29,8 +29,8 @@ def search(request):
     bedrooms = request.GET.get('bedrooms', 0)
     beds = request.GET.get('beds', 0)
     baths = request.GET.get('baths', 0)
-    instant = request.GET.get('instant', False)
-    super_host = request.GET.get('super_host', False)
+    instant = bool(request.GET.get('instant', False))
+    superhost = bool(request.GET.get('superhost', False))
     s_amenities = request.GET.getlist('amenities')
     s_facilities = request.GET.getlist('facilities')
 
@@ -46,7 +46,7 @@ def search(request):
         's_amenities' : s_amenities,
         's_facilities' : s_facilities,
         'instant' : instant,
-        'super_host' : super_host
+        'superhost' : superhost
     }
 
     room_types = models.RoomType.objects.all()
@@ -69,7 +69,36 @@ def search(request):
 
     if room_type != 0:
         filter_args['room_type__pk'] = room_type
+
+    if price != 0:
+        filter_args['price__lte'] = price
     
+    if guests != 0:
+        filter_args['guests__gte'] = guests
+
+    if bedrooms != 0:
+        filter_args['bedrooms__gte'] = bedrooms
+    
+    if beds != 0:
+        filter_args['beds__gte'] = beds
+
+    if baths != 0:
+        filter_args['baths__gte'] = baths
+    
+    if instant is True:
+        filter_args['instant_book'] = True
+
+    if superhost is True:
+        filter_args['host__superhost'] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args['amenities__pk'] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args['facilities__pk'] = int(s_facility)
+
     rooms = models.Room.objects.filter(**filter_args)
 
     return render(request, 'rooms/search.html', {**form, **choices, 'rooms' : rooms})
