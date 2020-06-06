2ad2e4c4b98157c4c36e8e2a9d4ebbccb54853ef
seon <seonu0823@gmail.com>
2020-06-06 18:00:19 +0900

    #13-9 Forms are Awesome!

--- rooms/forms.py
+++ rooms/forms.py
@@ -2,14 +2,13 @@ from django import forms
 from django_countries.fields import CountryField
 from . import models
 
+
 class SearchForm(forms.Form):
 
-    city = forms.CharField(initial='Anywhere')
-    country = CountryField(default='KR').formfield()
+    city = forms.CharField(initial="Anywhere")
+    country = CountryField(default="KR").formfield()
     room_type = forms.ModelChoiceField(
-        required=False,
-        empty_label='Any kind',
-        queryset=models.RoomType.objects.all()
+        required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
     )
     price = forms.IntegerField(required=False)
     guests = forms.IntegerField(required=False)
@@ -18,5 +17,13 @@ class SearchForm(forms.Form):
     baths = forms.IntegerField(required=False)
     instant_book = forms.BooleanField(required=False)
     superhost = forms.BooleanField(required=False)
-    amenities = forms.ModelMultipleChoiceField(queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple)
-    facilities = forms.ModelMultipleChoiceField(queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple)
+    amenities = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=models.Amenity.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    facilities = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=models.Facility.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
