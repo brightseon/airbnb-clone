270937a6b595c9042e6707a728e1425bee3445b8
seon <seonu0823@gmail.com>
2020-10-31 22:50:45 +0900

    #17-3 Recap OMG

--- users/views.py
+++ users/views.py
@@ -70,53 +70,60 @@ def github_login(request):
     redirect_url = 'http://127.0.0.1:8000/users/login/github/callback'
     return redirect(f'https://github.com/login/oauth/authorize?client_id={client_id}&redirect_url={redirect_url}&scope=read:user')
 
+class GithubException(Exception):
+    pass
+
 def github_callback(request):
-    client_id = os.environ.get('GH_ID')
-    client_secret = os.environ.get('GH_SECRET')
-    code = request.GET.get('code', None)
+    try:
+        client_id = os.environ.get('GH_ID')
+        client_secret = os.environ.get('GH_SECRET')
+        code = request.GET.get('code', None)
 
-    if code is not None:
-        request = requests.post(
-            f'https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}',
-            headers={'Accept': 'application/json'}
-        )
-        request_json = request.json()
-
-        error = request_json.get('error', None)
-
-        if error is not None:
-            return redirect(reverse('users:login'))
-        else:
-            access_token = request_json.get('access_token')
-            profile_request = requests.get(
-                f'https://api.github.com/user',
-                headers={
-                    'Authorization': f'token {access_token}',
-                    'Accept': 'application/json'
-                }
+        if code is not None:
+            request = requests.post(
+                f'https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}',
+                headers={'Accept': 'application/json'}
             )
+            request_json = request.json()
 
-            profile_json = profile_request.json()
-            username = profile_json.get('login', None)
+            error = request_json.get('error', None)
 
-            if username is not None:
-                name = profile_json.get('name')
-                email = profile_json.get('email')
-                bio = profile_json.get('bio')
-                user = models.User.objects.get(email=email)
-
-                if user is not None:
-                    return redirect(reverse('users:login'))
-                else:
-                    user = models.User.objects.create(
-                        username=email,
-                        first_name=name,
-                        bio=bio,
-                        email=email
-                    )
-                    login(request, user)
-                    return redirect(reverse('core:home'))
+            if error is not None:
+                raise GithubException()
             else:
-                return redirect(reverse('users:login'))
-    else:
-        return redirect(reverse('core:home'))
+                access_token = request_json.get('access_token')
+                profile_request = requests.get(
+                    f'https://api.github.com/user',
+                    headers={
+                        'Authorization': f'token {access_token}',
+                        'Accept': 'application/json'
+                    }
+                )
+
+                profile_json = profile_request.json()
+                username = profile_json.get('login', None)
+
+                if username is not None:
+                    name = profile_json.get('name')
+                    email = profile_json.get('email')
+                    bio = profile_json.get('bio')
+                    user = models.User.objects.get(email=email)
+
+                    if user is not None:
+                        return redirect(reverse('users:login'))
+                    else:
+                        user = models.User.objects.create(
+                            username=email,
+                            first_name=name,
+                            bio=bio,
+                            email=email
+                        )
+                        login(request, user)
+                        return redirect(reverse('core:home'))
+                else:
+                    raise GithubException()
+        else:
+            raise GithubException()
+    
+    except GithubException:
+        return redirect(reverse('users:login'))
