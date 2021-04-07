cbe8e403594ac396d33bfa129ca88506cb9abe50
seon-u <seonu0823@gmail.com>
2021-04-07 23:20:46 +0900

    #21-9 Mixins are Awesome

--- users/views.py
+++ users/views.py
@@ -8,9 +8,10 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.base import ContentFile
 from django.contrib import messages
-from . import forms, models
+from django.contrib.messages.views import SuccessMessageMixin
+from . import forms, models, mixins
 
-class LoginView(FormView) :
+class LoginView(mixins.LoggedOutOnlyView, FormView) :
 
     template_name = 'users/login.html'
     form_class = forms.LoginForm
@@ -32,7 +33,7 @@ def log_out(request):
     return redirect(reverse('core:home'))
 
 
-class SignUpView(FormView):
+class SignUpView(mixins.LoggedOutOnlyView, FormView):
     template_name = 'users/signup.html'
     form_class = forms.SignUpForm
     success_url = reverse_lazy('core:home')
@@ -220,7 +221,7 @@ class UserProfileView(DetailView):
     model = models.User
     context_object_name = 'user_obj'
 
-class UpdateProfileView(UpdateView):
+class UpdateProfileView(SuccessMessageMixin, UpdateView):
     
     model = models.User
     template_name = 'users/update-profile.html'
@@ -241,14 +242,17 @@ class UpdateProfileView(UpdateView):
     def get_form(self, form_class=None):
         form = super().get_form(form_class=form_class)
         
-        form.fields['birthdate'].widget.attrs = {'placeholder' : 'Birthdate'}
         form.fields['first_name'].widget.attrs = {'placeholder' : 'First name'}
+        form.fields['last_name'].widget.attrs = {'placeholder' : 'Last name'}
+        form.fields['bio'].widget.attrs = {'placeholder' : 'Bio'}
+        form.fields['birthdate'].widget.attrs = {'placeholder' : 'Birthdate'}
         
         return form
 
-class UpdatePasswordView(PasswordChangeView):
+class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
     
     template_name = 'users/update-password.html'
+    success_message = 'Password Updated'
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class=form_class)
@@ -259,3 +263,6 @@ class UpdatePasswordView(PasswordChangeView):
         }
         
         return form
+    
+    def get_success_url(self):
+        return self.request.user.get_absolute_url()
